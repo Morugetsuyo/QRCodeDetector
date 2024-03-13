3f87350ac88548a5da5cc4303c9ebd6f00bf1458
Morugetsuyo <82621702+Morugetsuyo@users.noreply.github.com>
2024-03-13 21:12:50 +0900

    prefix sender (background.js)

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 'use strict';
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
   if (request.action === "captureTab") {
     chrome.tabs.captureVisibleTab(null, { format: 'png' }, (dataUrl) => {
       if (chrome.runtime.lastError) {
